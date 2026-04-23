e372b37122d7b93e69ace9f15530cf196aff63d1
finnblockz-cloud <finnblockz@gmail.com>
2026-04-23 18:03:18 +0500

    fix: hardcode recipient for testing

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -43,7 +43,7 @@ def send_whatsapp_message(to, message):
     }
     data = {
         "messaging_product": "whatsapp",
-        "to": to,
+        "to": "77771601111",
         "type": "text",
         "text": {"body": message}
     }
